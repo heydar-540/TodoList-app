d549003297f222a8d30ca16be9ad48455a36e1f3
Ahmad <ahmad12ahmadi12@gmail.com>
2022-05-31 19:19:02 +0430

    inital commit

---
+++ todolist.js
@@ -0,0 +1,100 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-btn");
+const todoList = document.querySelector(".todolist");
+const filterOption = document.querySelector(".filter-todos");
+
+// event listener
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", checkRemove);
+filterOption.addEventListener("click", filterTodos);
+document.addEventListener('DOMContentLoaded', getLocalTodos);
+
+// functions
+function addTodo(e) {
+  e.preventDefault();
+  //   console.log(e);
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  const newTodo = ` <li>${todoInput.value}</li>
+    <span><i class="fa-solid fa-square-check"></i></span>
+    <span><i class="fa-solid fa-trash-can"></i></span>`;
+  todoDiv.innerHTML = newTodo;
+  todoList.appendChild(todoDiv);
+  saveLocalTodos(todoInput.value);
+  todoInput.value = "";
+}
+
+function checkRemove(e) {
+  const classList = [...e.target.classList];
+  const item = e.target;
+  console.log(item.parentElement.parentElement);
+  if (classList[1] === "fa-square-check") {
+    const todo = item.parentElement.parentElement;
+    todo.classList.toggle("completed");
+  } else if (classList[1] === "fa-trash-can") {
+    const todo = item.parentElement.parentElement;
+    removeLocalTodos(todo);
+    todo.remove();
+  }
+}
+
+function filterTodos(e) {
+  const todos = [...todoList.childNodes];
+  todos.forEach((todo) => {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.diplay = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.diplay = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+function saveLocalTodos(todo) {
+  // localStorage.getItem('todos')
+  let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+    savedTodos.push(todo);
+    localStorage.setItem('todos',JSON.stringify(savedTodos));
+}
+
+
+function getLocalTodos(todo) {
+  
+  let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+    savedTodos.forEach(todo=>{
+      const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  const newTodo = ` <li>${todo}</li>
+    <span><i class="fa-solid fa-square-check"></i></span>
+    <span><i class="fa-solid fa-trash-can"></i></span>`;
+  todoDiv.innerHTML = newTodo;
+  todoList.appendChild(todoDiv);
+    });
+}
+
+function removeLocalTodos(todo){
+  let savedTodos = localStorage.getItem("todos")
+  ? JSON.parse(localStorage.getItem("todos"))
+  : [];
+  const filteredTodos = savedTodos.filter((t)=> t!== todo.children[0].innerText);
+  localStorage.setItem('todos',JSON.stringify(filteredTodos));
+}
